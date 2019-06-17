912223cf9a9649f355d57506cc18fb6e1ccd0af1
mahatmaWM <wangming_0304@163.com>
2019-06-17 16:44:03 +0800

    run_demo.sh

--- run_demo.sh
+++ run_demo.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-python main.py --status train --train ./data/demo.train.char --dev ./data/demo.dev.char --test ./data/demo.test.char --savemodel ./data/demo
-
-#python main.py --status train --train ./data/demo.train.char --dev ./data/demo.dev.char --test ./data/demo.test.char --savemodel ./data/demo > ./demo.log 2>&1 &
+python main.py --status train --train ./data/train.txt --dev ./data/dev.txt --test ./data/test.txt --savemodel ./data/test
 
 #python main.py --status train --train ./data/train.txt --dev ./data/dev.txt --test ./data/test.txt --savemodel ./data/fm-play > ./music-play.log 2>&1 &
-
-#python main.py --status train --train ./data/music.play.train --dev ./data/music.play.dev --test ./data/music.play.test --savemodel ./data/music-play > ./music-play.log 2>&1 &
-
-# python2 main.py --status decode --raw ../data/onto4ner.cn/demo.test.char --savedset ../data/onto4ner.cn/demo.dset --loadmodel ../data/onto4ner.cn/demo.0.model --output ../data/onto4ner.cn/demo.raw.out
